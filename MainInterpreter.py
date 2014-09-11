b5cc62eb30579c199c45b7adaccacd138a7e99c2
Dan Thompson <danthompson41@gmail.com>
2014-09-10 22:15:36 -0300

    Fixed Numpy Dependances
    It was broken when it was kept in the Reference Files. Don't know why.
    Accepted and moved on, moved to the main folder.

--- MainInterpreter.py
+++ MainInterpreter.py
@@ -3,9 +3,10 @@ import re
 import time
 
 ###### Reference files from interwebz #######
+
 from References import serial
 from References import matplotlib
-from References import numpy as np
+import numpy as np
 from References.matplotlib import pyplot as plt
 from References.matplotlib import animation as animation
 
